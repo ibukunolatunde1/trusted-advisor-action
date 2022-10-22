696f7af8c5672f2bc26ba10ee1ccb6fae4671d67
Ibukunoluwa Olatunde <ctw02127@CTW0212dminsMBP.lan>
2022-10-22 23:24:24 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import core from '@actions/core'
+import github from '@actions/github'
+import { Support, STS } from 'aws-sdk'
+
+const main = async () => {
+    try {
+        const email = core.getInput('email', { required: true });
+        const profile = core.getInput('email', { required: true });
+        const region = core.getInput('email', { required: true });
+        console.log(`Hello ${email} this is your ${profile}`);
+        const time = (new Date()).toTimeString();
+        core.setOutput("time", time);
+        const support = new Support();
+        let params = {
+            language: 'en',
+            region: `${region}`
+        }
+        support.describeTrustedAdvisorChecks(params, (err, data) => {
+            if(err) console.log(err, err.stack);
+            else console.log(data);
+        })
+    } catch (error) {
+        console.error(error);
+        core.setFailed(error.message);
+    }
+}
+
+main();
