27bc5dd7bdf817a478904b06b7da24dc6e4477ef
Ibukunoluwa Olatunde <ibukunoluwa.adedolapo@ctw.bmwgroup.com>
2022-10-23 21:01:34 +0100

    Added INput

--- index.js
+++ index.js
@@ -6,34 +6,53 @@ const fs = require('fs');
 
 const main = async () => {
     try {
-        // const email = core.getInput('email');
-        // const profile = core.getInput('profile');
-        // const region = core.getInput('region');
-        const email = 'ibukunolatunde1@gmai.com'
-        const profile = 'Production'
-        const region = 'us-east-1'
+        const email = core.getInput('email');
+        const profile = core.getInput('profile');
+        const region = core.getInput('region');
+        // const email = 'ibukunolatunde1@gmai.com'
+        // const profile = 'Production'
+        // const region = 'eu-central-1'
         console.log(`Hello ${email} this is your ${profile}`);
         const time = (new Date()).toTimeString();
-        // core.setOutput("time", time);
+        core.setOutput("time", time);
+        AWS.config.update({region: region});
         const support = new AWS.Support();
         let params = {
-            language: 'en',
-            region: `${region}`
+            language: 'en'
         }
         support.describeTrustedAdvisorChecks(params, (err, data) => {
             if(err) console.log(err, err.stack);
             else console.log(data);
             let output = new PDFGenerator
             output.pipe(fs.createWriteStream('trusted_advisor_checks.pdf'));
-            output.text('Sample Text', { bold: true, underline: true, align: center });
-            output.text(data);
+            output.text('Sample Text', { bold: true, underline: true, align: 'center' });
+            // generateTable(output, data);
             output.end()
         })
         
     } catch (error) {
         console.error(error);
-        // core.setFailed(error.message);
+        core.setFailed(error.message);
     }
 }
+
+// const generateTable = (outputStream, data) => {
+//     const tableTop = 270
+//     const nameCodeX = 50
+//     const descriptionX = 250
+//     const categoryX = 350
+//     const metadataX = 500
+
+//     outputStream.fontSize(10)
+//         .text('Name', nameCodeX, tableTop, {bold: true})
+//         .text('Description', descriptionX, tableTop, {bold: true})
+//         .text('Category', categoryX, tableTop, {bold: true})
+//         .text('Metadata', metadataX, tableTop, {bold: true})
+    
+//     for(let i = 0; i < data.checks.length; i++){
+
+//     }
+    
+// } 
 
 main();
